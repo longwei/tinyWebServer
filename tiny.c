a1cc2c4ab732767d9d467e818343dac1be4d5563
Longwei Su <longwei.su@gmail.com>
2014-09-10 00:34:10 -0400

    replace with more secure method

--- tiny.c
+++ tiny.c
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define VERSION 23
 #define BUFSIZE 8096
@@ -16,6 +17,7 @@
 #define LOG        44
 #define FORBIDDEN 403
 #define NOTFOUND  404
+#define BACKLOG_PENNDING_CONNECTION 64
 
 struct {
     char *ext;
@@ -35,27 +37,32 @@ struct {
 
 void logger(int type, char *s1, char *s2, int socket_fd)
 {
-    int fd ;
-    char logbuffer[BUFSIZE*2];
+    int log_fd ;
+    char logbuffer[BUFSIZE+1];
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
 
     switch (type) {
-        case ERROR: sprintf(logbuffer,"ERROR: %s:%s Errno=%d exiting pid=%d",s1, s2, errno,getpid());
+        case ERROR: snprintf(logbuffer, BUFSIZE, "%sERROR: %s:%s Errno=%d exiting pid=%d",asctime(timeinfo), s1, s2, errno,getpid());
             break;
         case FORBIDDEN:
             write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n",271);
-            sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2);
+            snprintf(logbuffer, BUFSIZE, "%sFORBIDDEN: %s:%s",asctime(timeinfo), s1, s2);
             break;
         case NOTFOUND:
             write(socket_fd, "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",224);
-            sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2);
+            snprintf(logbuffer, BUFSIZE, "%sNOT FOUND: %s:%s",asctime(timeinfo), s1, s2);
             break;
-        case LOG: sprintf(logbuffer," INFO: %s:%s:%d",s1, s2,socket_fd); break;
+        case LOG: snprintf(logbuffer, BUFSIZE,"%sINFO: %s:%s:%d\n",asctime(timeinfo), s1, s2,socket_fd); break;
     }
-    /* No checks here, nothing can be done with a failure anyway */
-    if((fd = open("nweb.log", O_CREAT| O_WRONLY | O_APPEND,0644)) >= 0) {
-        write(fd,logbuffer,strlen(logbuffer));
-        write(fd,"\n",1);
-        close(fd);
+
+    if((log_fd = open("tiny.log", O_CREAT| O_WRONLY | O_APPEND, 0644)) >= 0) {
+        write(log_fd,logbuffer,strlen(logbuffer));
+        write(log_fd,"\n",1);
+        close(log_fd);
     }
     if(type == ERROR || type == NOTFOUND || type == FORBIDDEN) exit(3);
 }
@@ -91,15 +98,17 @@ void web(int fd, int hit)
         }
     }
     //todo
-    //"//etc/foobar" will bypass this check
-    for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
+    // "//etc/foobar" will bypass this check
+    // check for illegal parent directory use ..
+    for(j=0;j<i-1;j++)
         if(buffer[j] == '.' && buffer[j+1] == '.') {
             logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
         }
+
     if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
         strcpy(buffer,"GET /index.html");
 
-    /* work out the file type and check we support it */
+    // double check the file type
     buflen=strlen(buffer);
     fstr = (char *)0;
     for(i=0;extensions[i].ext != 0;i++) {
@@ -115,11 +124,11 @@ void web(int fd, int hit)
         logger(NOTFOUND, "failed to open file",&buffer[5],fd);
     }
     logger(LOG,"SEND",&buffer[5],hit);
-//    lseek to the file end to find the length
+    //lseek to the file end to find the length
     len = (long)lseek(file_fd, (off_t)0, SEEK_END);
     //lseek back to the file start ready for reading
     lseek(file_fd, (off_t)0, SEEK_SET);
-    sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */
+    snprintf(buffer, BUFSIZE,"HTTP/1.1 200 OK\nServer: tiny/%d.0\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */
     logger(LOG,"Header",buffer,hit);
     //sending header
     write(fd,buffer,strlen(buffer));
@@ -128,7 +137,8 @@ void web(int fd, int hit)
     while (	(ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
         write(fd,buffer,ret);
     }
-    sleep(1);	/* allow socket to drain before signalling the socket is closed */
+    //some os may not wait for socket to finished sending the data but drop connection
+    sleep(1);
     close(fd);
     exit(1);
 }
@@ -142,15 +152,15 @@ int main(int argc, char **argv)
 
     argv[1] = "8081";
     argv[2] = ".";
-    /* Become deamon + unstopable and no zombies children (= no wait()) */
+    // Become deamon
     if(fork() != 0)
-        return 0; /* parent returns OK to shell */
+        return 0;
 
     signal(SIGCHLD, SIG_IGN); /* ignore child death */
     signal(SIGHUP, SIG_IGN); /* ignore terminal hangups */
     setpgrp();		/* break away from process group */
 
-    logger(LOG,"nweb starting",argv[1],getpid());
+    logger(LOG,"tiny starting",argv[1],getpid());
     /* setup the network socket */
     if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0)
         logger(ERROR, "system call","socket",0);
@@ -164,9 +174,10 @@ int main(int argc, char **argv)
 
     if(bind(listenfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr)) <0)
         logger(ERROR,"system call","bind",0);
-    if( listen(listenfd,64) <0)
+    if( listen(listenfd,BACKLOG_PENNDING_CONNECTION) <0)
         logger(ERROR,"system call","listen",0);
 
+    logger(LOG,"starting","listen",0);
     for(hit=1; ;hit++) {
         length = sizeof(cli_addr);
         if((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length)) < 0)
